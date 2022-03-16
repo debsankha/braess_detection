850f101dedb746c6fd86ac2e6f62816f5c09f365
Debsankha Manik <debsankha.manik@moia.io>
2022-03-16 11:51:27 +0100

    setup.py specifies dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="braess_detection",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "networkx==2.0",
+        "numpy",
+        "pandas",
+        "seaborn",
+        "flownetpy",
+        "pytest",
+        "jupyterlab",
+        "jupyterlab_vim",
+        "tqdm",
+        "loky",
+    ],
 )
